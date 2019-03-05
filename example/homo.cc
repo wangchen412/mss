ad98d5e923d23fcbb61e447877455e2914559764
Chen Wang <wangchen412@gmail.com>
2019-03-05 10:28:53 -0700

    Homogenization example with frequency input.

--- example/homo.cc
+++ example/homo.cc
@@ -52,62 +52,12 @@ class Mismatch {
   const Material m0_;
 };
 
-double BoundaryMismatch(const Eigen::VectorXcd& w, const Eigen::VectorXcd& t,
-                        const Material& material) {
-  Matrix m(material, 16576.2);
-  Boundary<AP, 4> b{500, {{-0.3, 0.3}, {0.3, -0.3}}, &m};
-
-  return (b.MatrixH() * w - b.MatrixG() * t).norm();
-}
-
-void Scan(const Material& m1, const Material& m2, size_t n) {
-  double dr_rho = (m2.Rho_comp() - m1.Rho_comp()).real() / n;
-  double di_rho = (m2.Rho_comp() - m1.Rho_comp()).imag() / n;
-
-  double dr_mu = (m2.Mu_comp() - m1.Mu_comp()).real() / n;
-  double di_mu = (m2.Mu_comp() - m1.Mu_comp()).imag() / n;
+int main(int argc, char** argv) {
+  if (argc != 2) exit_error_msg({"Frequency required."});
 
   Eigen::VectorXcd w(1200), t(1200);
   ReadBv(w, t);
-
-  double rst[n][n][n][n];
-
-#ifdef NDEBUG
-#pragma omp parallel for
-#endif
-  for (size_t jj = 0; jj < n * n; jj++) {
-    size_t j = jj % n;
-    size_t i = (jj - j) / n;
-    for (size_t k = 0; k < n; k++)
-      for (size_t l = 0; l < n; l++) {
-        Material m({m1.Rho_comp().real() + i * dr_rho,
-                    m1.Rho_comp().imag() + j * di_rho},
-                   0,
-                   {m1.Mu_comp().real() + k * dr_mu,
-                    m1.Mu_comp().imag() + l * di_mu});
-        rst[i][j][k][l] = BoundaryMismatch(w, t, m);
-      }
-  }
-
-  std::ofstream file("mismatch.dat");
-  for (size_t i = 0; i < n; i++)
-    for (size_t j = 0; j < n; j++)
-      for (size_t k = 0; k < n; k++)
-        for (size_t l = 0; l < n; l++)
-          file << setMaxPrecision << m1.Rho_comp().real() + i * dr_rho << "\t"
-               << m1.Rho_comp().imag() + j * di_rho << "\t"
-               << m1.Mu_comp().real() + k * dr_mu << "\t"
-               << m1.Mu_comp().imag() + l * di_mu << "\t" << rst[i][j][k][l]
-               << std::endl;
-
-  file.close();
-}
-
-int main() {
-  Eigen::VectorXcd w(1200), t(1200);
-  ReadBv(w, t);
-
-  Mismatch f(16576.2, w, t, {{11400, 11400}, 0, {84e9, 84e9}});
+  Mismatch f(atof(argv[1]), w, t, {{11400, 11400}, 0, {84e9, 84e9}});
   std::ofstream file("iterations.dat");
   GradientDescent(f, &file);
   return 0;
